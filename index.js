7a5c09d47b1dd16b86779923b8ece52a1d83b0ba
Andrei Kashcha <anvaka@gmail.com>
2020-07-03 13:10:07 -0700

    maybe it will rebuild now?

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ let executeOSMQuery = require('./lib/executeOSMQuery')
 var JSONStream = require('JSONStream')
 var es = require('event-stream')
 
+
 // readErroredFile(outFileName, crawl); 
 
 readProcessedFile(outFileName, crawl);
